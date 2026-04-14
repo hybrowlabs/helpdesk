7bcd93f7839441a5a9705c51c9007c0c17956a33
Ajay <ajayjogdand374@gmail.com>
2026-04-14 10:42:17 +0530

    fix:SLA

--- helpdesk/patches/create_missing_custom_fields.py
+++ helpdesk/patches/create_missing_custom_fields.py
@@ -1,8 +1,19 @@
-from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+import frappe
 
 from helpdesk.setup.install import get_custom_fields
 
 
 def execute():
-    """Create any custom fields defined in get_custom_fields() that are missing on the site."""
-    create_custom_fields(get_custom_fields(), ignore_validate=True)
+    """Create custom fields defined in get_custom_fields() only if they don't already exist."""
+    for doctype, fields in get_custom_fields().items():
+        for field in fields:
+            fieldname = field.get("fieldname")
+            if not frappe.db.exists("Custom Field", {"dt": doctype, "fieldname": fieldname}):
+                frappe.get_doc(
+                    {
+                        "doctype": "Custom Field",
+                        "dt": doctype,
+                        **field,
+                    }
+                ).insert(ignore_permissions=True)
+                frappe.db.commit()
